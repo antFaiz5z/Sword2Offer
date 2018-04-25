7a86766164de67ba73e8a732b22612db745da70f
antFaiz5z <antFaiz5z@qq.com>
2018-04-25 13:27:19 +0800

    normal add

--- List.h
+++ List.h
@@ -14,6 +14,11 @@ class List {
 
 public:
 
+    /*
+     * 时间限制：1秒 空间限制：32768K 热度指数：496961
+     * 本题知识点： 链表
+     * 输入一个链表，从尾到头打印链表每个节点的值。
+     */
     std::vector<int> printListFromTailToHead(ListNode* head);
 
     static void local_main_printListFromTailToHead();
