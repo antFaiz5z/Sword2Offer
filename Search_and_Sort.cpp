1695f56d7021d6b0e814e73c8acbcf2243f011c6
antFaiz5z <antFaiz5z@qq.com>
2018-05-12 11:31:24 +0800

    normal commit

--- Search_and_Sort.cpp
+++ Search_and_Sort.cpp
@@ -24,15 +24,74 @@ int Search_and_Sort::minNumberInRotateArray(std::vector<int> rotateArray) {
     return -1;
 }
 
+int Search_and_Sort::minNumberInRotateArray_II(std::vector<int> rotateArray) {
+
+    if(rotateArray.empty()){
+        return 0;
+    }
+    int left = 0;
+    int right = static_cast<int>(rotateArray.size() - 1);
+    return find(rotateArray, left, right);
+}
+
+int Search_and_Sort::find(std::vector<int> &rotateArray, int left, int right) {
+
+    int mid = (left + right) /2;
+    if(rotateArray[mid] < rotateArray[left] && rotateArray[mid] < rotateArray[right]){
+        find(rotateArray, left, mid);
+    }else if(rotateArray[mid] > rotateArray[left] && rotateArray[mid] > rotateArray[right]){
+        find(rotateArray, mid, right);
+    }else if(mid == left){
+        return rotateArray[right];
+    }
+}
+
+int Search_and_Sort::minNumberInRotateArray_standard(std::vector<int> rotateArray) {
+
+    if(rotateArray.empty()){
+        return 0;
+    }
+    int left = 0;
+    int right = static_cast<int>(rotateArray.size() - 1);
+    int mid = 0;
+
+    while(rotateArray[left] >= rotateArray[right]){
+
+        if((right - left) == 1){
+            mid = right;
+            break;
+        }
+        mid = (left + right) /2;
+        if(rotateArray[left] == rotateArray[mid] && rotateArray[left] == rotateArray[right]){
+            find_min(rotateArray, left, right);
+        }
+        if(rotateArray[mid] >= rotateArray[left]){
+            left = mid;
+        }else{
+            right = mid;
+        }
+    }
+    return rotateArray[mid];
+}
+
+int Search_and_Sort::find_min(std::vector<int> &rotateArray, int left, int right) {
+
+    int ret = rotateArray[0];
+    for (int i = 1; i < rotateArray.size() - 1; ++i) {
+        if(ret > rotateArray[i]){
+            ret = rotateArray[i];
+        }
+    }
+    return ret;
+}
+
 void Search_and_Sort::local_main_minNumberInRotateArray() {
 
-    std::vector<int> a;
-    for (int i = 3; i < 10; ++i) {
-        a.push_back(i);
-    }
-    for (int i = 1; i < 3; ++i) {
-        a.push_back(i);
-    }
+    //std::vector<int> a = {4,5,6,7,8,9,1,2,3};
+    std::vector<int> a = {4,5,6,7,8,8,1,3,3};
+
     auto * main = new Search_and_Sort();
     std::cout << main->minNumberInRotateArray(a) << std::endl;
+    std::cout << main->minNumberInRotateArray_II(a) << std::endl;
+    std::cout << main->minNumberInRotateArray_standard(a) << std::endl;
 }
--- Search_and_Sort.h
+++ Search_and_Sort.h
@@ -10,7 +10,14 @@
 class Search_and_Sort {
 
 public:
-    int minNumberInRotateArray(std::vector<int> rotateArray);
+    int minNumberInRotateArray(std::vector<int> rotateArray);//效率较低
+
+    int minNumberInRotateArray_II(std::vector<int> rotateArray);//无法应对存在相等数据情况
+    int find(std::vector<int> &rotateArray, int left, int right);
+
+    int minNumberInRotateArray_standard(std::vector<int> rotateArray);
+    int find_min(std::vector<int> &rotateArray, int left, int right);
+
     static void local_main_minNumberInRotateArray();
 
 
