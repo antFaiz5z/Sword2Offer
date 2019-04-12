f53270d599b847a8d2a497320dfb60540ae5d9df
antFaiz5z <antFaiz5z@gmail.com>
2019-04-12 21:23:59 +0800

    normal commit

--- Test/main.cpp
+++ Test/main.cpp
@@ -7,10 +7,87 @@
 #include <limits>
 #include <algorithm>
 #include <cstring>
+#include <list>
 
 using namespace std;
 
+struct TimeSegment {
+    uint64_t btime;
+    uint64_t etime;
+
+    TimeSegment() : btime(0), etime(0) {
+    }
+
+    TimeSegment(uint64_t btime_, uint64_t etime_) : btime(btime_), etime(etime_) {
+    }
+};
+
+
+void show(std::list<TimeSegment> &lst){
+
+    cout << "---------------" <<endl;
+    for (auto &i : lst){
+        cout << i.btime << ", " << i.etime << endl;
+    }
+}
+
+inline void MergeTimeSegment(uint64_t nBeginTime, uint64_t nEndTime, std::list<TimeSegment> &lstTimeSegment) {
+
+    for (auto it = lstTimeSegment.begin(); it != lstTimeSegment.end(); it++) {
+
+        if (nBeginTime > it->etime) {
+            continue;
+        }
+        if (nEndTime < it->btime) {
+            lstTimeSegment.insert(it, TimeSegment(nBeginTime, nEndTime));
+            return;
+        }
+        if (nBeginTime >= it->btime && nEndTime <= it->etime) {
+            return;
+        }
+        if (nBeginTime < it->btime) {
+            it->btime = nBeginTime;
+            if (nEndTime >= it->btime && nEndTime <= it->etime) {
+                return;
+            }
+        }
+
+        auto it3 = it;
+        auto it2 = ++it3;
+        for (; it2 != lstTimeSegment.end();) {
+            show(lstTimeSegment);
+            if (nEndTime >= it2->etime) {
+                it->etime = nEndTime;
+                it2 = lstTimeSegment.erase(it2);
+            } else if (nEndTime >= it2->btime && nEndTime < it2->etime) {
+                it->etime = it2->etime;
+                lstTimeSegment.erase(it2);
+                return;
+            } else {
+                it->etime = nEndTime;
+                return;
+            }
+        }
+    }
+
+
+}
+
+
 int main() {
+
+    std::list<TimeSegment> lst;
+    lst.emplace_back(10,20);
+    lst.emplace_back(40,50);
+    lst.emplace_back(60,70);
+    lst.emplace_back(80,90);
+    lst.emplace_back(100,110);
+
+    show(lst);
+
+    MergeTimeSegment(20, 40, lst);
+
+    show(lst);
 
     /*int n, begin, end;
     int max = 0, min = numeric_limits<int32_t>::max();
@@ -49,12 +126,9 @@ int main() {
     }
     cout << ret << endl;*/
 
-    string str;
-    cin >> str;
-    bool first = true;
 
-    for (int i = 0; i < str.size(); ++i) {
 
-    }
+
 
 }
+
