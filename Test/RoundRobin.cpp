85e6a23fd3eca7f74fd1583e8f6d84e2c73d9e88
antfaiz5z <antfaiz5z@gmail.com>
2019-04-25 20:55:06 +0800

    modify cmake structure

--- Test/RoundRobin.cpp
+++ Test/RoundRobin.cpp
@@ -3,3 +3,58 @@
 //
 
 #include "RoundRobin.h"
+
+#include <iostream>
+
+int RoundRobin::get_index() {
+    index = (index + 1) % size;
+    return index;
+}
+
+SmoothWeightedRoundRobin::SmoothWeightedRoundRobin(const std::vector<entity> &entities) {
+    total_weight = 0;
+    for (auto &i : entities) {
+        servers.push_back(server{i.first, 0, i.second});
+        total_weight += i.first;
+    }
+    size = entities.size();
+}
+
+int SmoothWeightedRoundRobin::get_index() {
+    int cur_index = -1;
+    int cur_weight = -1;
+
+    for (int i = 0; i < size; ++i) {
+        servers[i].cur_weight += servers[i].weight;
+        if (servers[i].cur_weight > cur_weight){
+            cur_weight = servers[i].cur_weight;
+            cur_index = i;
+        }
+    }
+    servers[cur_index].cur_weight -= total_weight;
+
+    return cur_index;
+}
+
+
+int main() {
+
+    int index;
+    std::vector<entity>  entitites;
+    entitites.emplace_back(entity(4, "a"));
+    entitites.emplace_back(entity(2, "b"));
+    entitites.emplace_back(entity(1, "c"));
+
+    SmoothWeightedRoundRobin swr = SmoothWeightedRoundRobin(entitites);
+    for (int i = 0; i < 14; ++i) {
+        index = swr.get_index();
+        std::cout << "selected server: " << index << " - " << swr.servers[index].name << std::endl << "cur_weights: | ";
+        for (auto &j : swr.servers){
+            std::cout << j.cur_weight << " | " ;
+        }
+        std::cout << std::endl;
+    }
+
+}
+
+
--- Test/RoundRobin.h
+++ Test/RoundRobin.h
@@ -6,16 +6,47 @@
 #define JIANZHIOFFER_ROUNDROBIN_H
 
 
+#include <string>
+#include <vector>
+
+
+typedef struct {
+    int weight;
+    int cur_weight;
+    std::string name;
+} server;
+
+typedef std::pair<int, std::string> entity;
+
 class RoundRobin {
 
+public:
+
+    RoundRobin() : size(0), index(0) {}
+
+    int get_index();
+
+private:
+    int size;
+    int index;
 };
 
 class WeightedRoundRobin {
-
 };
 
 class SmoothWeightedRoundRobin {
 
+public:
+
+    explicit SmoothWeightedRoundRobin(const std::vector<entity> &entities);
+
+    int get_index();
+
+    std::vector<server> servers;
+
+private:
+    int size;
+    int total_weight;
 };
 
 
