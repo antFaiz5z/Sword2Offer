e8f2fce89fc264daa0a95747bc29b662886e503b
antFaiz5z <antFaiz5z@gmail.com>
2019-04-29 20:59:27 +0800

    add tree

---
+++ LeetCode/DataStructure/Tree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by faiz on 19-4-29.
+//
+
+#include "Tree.h"
---
+++ LeetCode/DataStructure/Tree.h
@@ -0,0 +1,45 @@
+//
+// Created by faiz on 19-4-29.
+//
+
+#ifndef JIANZHIOFFER_TREE_H
+#define JIANZHIOFFER_TREE_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Trie {
+public:
+    /** Initialize your data structure here. */
+    Trie() {
+
+    }
+
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+    }
+
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+
+    }
+
+private:
+    vector<string> vs;
+
+};
+
+
+class Tree {
+
+};
+
+
+#endif //JIANZHIOFFER_TREE_H
