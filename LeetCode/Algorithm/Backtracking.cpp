8c4fcbc7c04739595956023ac861fe5c06a30f5c
antFaiz5z <antFaiz5z@gmail.com>
2019-06-25 15:37:34 +0800

    normal commit

--- LeetCode/Algorithm/Backtracking.cpp
+++ LeetCode/Algorithm/Backtracking.cpp
@@ -32,5 +32,83 @@ vector<int> Backtracking::grayCodeII(int n) {
     return result;
 }
 
+vector<vector<string>> Backtracking::solveNQueens(int n) {
+
+    vector<vector<pair<int, int>>> solutions;
+    vector<pair<int, int>> solution;
+
+    solveNQueens_backtracking(solutions, solution, 0, n);
+
+    return draw(solutions, n);
+}
+
+void
+Backtracking::solveNQueens_backtracking(vector<vector<pair<int, int>>> &solutions, vector<pair<int, int>> &solution,
+                                        int row, int n) {
+
+    if (row == n) solutions.push_back(solution);
+    for (int col = 0; col < n; ++col) {
+        if (isValid(solution, row, col)) {
+            solution.emplace_back(row, col);
+            solveNQueens_backtracking(solutions, solution, row + 1, n);
+            solution.pop_back();
+        }
+    }
+}
+
+bool Backtracking::isValid(vector<pair<int, int>> &solution, int row, int col) {
+
+    for (pair<int, int> &i : solution)
+        if (i.first == row || i.second == col || i.first + i.second == row + col || i.first - i.second == row - col)
+            return false;
+    return true;
+}
+
+vector<vector<string>> Backtracking::draw(vector<vector<pair<int, int>>> &solutions, int n) {
+
+    string stmp(n, '.');
+    vector<string> tmp(n, stmp);
+    vector<vector<string>> ret;
+    for (vector<pair<int, int>> &solution : solutions) {
+        vector<string> one(tmp);
+        for (pair<int, int> &i : solution) {
+            one[i.first][i.second] = 'Q';
+        }
+        ret.push_back(one);
+    }
+    return ret;
+}
+
+int Backtracking::totalNQueens(int n) {
+
+    vector<int> queensInCol(n);
+    int sum = 0;
+
+    totalNQueens_backtracking(queensInCol, n, 0, sum);
+    return sum;
+}
+
+void Backtracking::totalNQueens_backtracking(vector<int> &queensInCol, int n, int row, int &sum) {
+
+    if (row == n) {
+        sum += 1;
+        return;
+    }
+    for (int i = 0; i < n; ++i) {
+        if (isValid(queensInCol, row, i)) {
+            queensInCol[row] = i;
+            totalNQueens_backtracking(queensInCol, n, row + 1, sum);
+        }
+    }
+}
+
+bool Backtracking::isValid(vector<int> &queensInCol, int row, int col) {
+
+    for (int i = 0; i < row; ++i) {
+        if (queensInCol[i] == col || abs(i - row) == abs(queensInCol[i] - col)) return false;
+    }
+    return true;
+}
+
 
 
--- LeetCode/Algorithm/Backtracking.h
+++ LeetCode/Algorithm/Backtracking.h
@@ -6,6 +6,7 @@
 #define SWORD2OFFER_BACKTRACKING_H
 
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -16,7 +17,25 @@ public:
     void grayCodeHelper(vector<int> &ret, unsigned int *now, unsigned int n);
 
     vector<int> grayCode(int n);
+
     vector<int> grayCodeII(int n);
+
+    //51
+    vector<vector<string>> solveNQueens(int n);
+
+    void solveNQueens_backtracking(vector<vector<pair<int, int>>> &solutions, vector<pair<int, int>> &solution,
+                                   int row, int n);
+
+    bool isValid(vector<pair<int, int>> &solution, int row, int col);
+
+    vector<vector<string>> draw(vector<vector<pair<int, int>>> &solutions, int n);
+
+    //52
+    int totalNQueens(int n);
+
+    void totalNQueens_backtracking(vector<int> &queensInCol, int n, int row, int &sum);
+
+    bool isValid(vector<int> &queensInCol, int row, int col);
 
 
 };
