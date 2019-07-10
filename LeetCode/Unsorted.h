0c7a85853216aeba0bc59da0f65e4af83379c803
antFaiz5z <antFaiz5z@gmail.com>
2019-07-10 15:22:41 +0800

    normal commit

--- LeetCode/Unsorted.h
+++ LeetCode/Unsorted.h
@@ -16,10 +16,9 @@ public:
     //1103
     vector<int> pathInZigZagTree(int label);
 
-    void bytedance_one();//error
-
+    //bytedance
+    void bytedance_one();//error 30%
     void bytedance_two();//error
-
     void bytedance_bt(vector<int> &score, vector<int> &divide, int n, int *max_score, int now_score, int direction, int index);
 
 };
